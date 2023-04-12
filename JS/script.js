2c76cb37e25ed64f892cf298e60283545680fc69
Carlosqmenezes <RM99849@fiap.com.br>
2023-04-11 21:02:15 -0300

    função exemplos

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,19 @@
 //exemplo de funções
 
+const divElement = document.getElementById('div-box')
+/*
+divElement.addEventListener("click", ()=>{
+    console.log(divElement)
+});
+*/
+divElement.addEventListener("click",mostraElemento);
+    function mostraElemento(){
+        console.log(divElement)
+    }
+
+    function soma (){
+        let v1 = 5
+        let v2 = 2
+        console.log(v1 + v2)
+
+    }
