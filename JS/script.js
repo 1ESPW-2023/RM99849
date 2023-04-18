11f97a138f59976f92c8e03eec31c8b144a23070
Carlosqmenezes <RM99849@fiap.com.br>
2023-04-18 20:57:59 -0300

    alteração

--- JS/script.js
+++ JS/script.js
@@ -1,47 +1,63 @@
-/*
-//Exemplo de Funções
-const divElement = document.getElementById("div-box")
+// //Exemplo de Funções
+// const divElement = document.getElementById("div-box")
 
-// divElement.addEventListener("click", ()=>{
-//     console.log(divElement);
-// });
+// // divElement.addEventListener("click", ()=>{
+// //     console.log(divElement);
+// // });
 
-divElement.addEventListener("click", mostraElemento);
+// divElement.addEventListener("click", mostraElemento);
 
-function mostraElemento() {
-    console.log(divElement)
-}
 
-function soma() {
-    let n1 = 10
-    let n2 = 5
-    console.log(n1 + n2)
-}
-*/
+// function mostraElemento() {
+//     console.log(divElement)
+// }
+
+// function soma() {
+//     let n1 = 10
+//     let n2 = 5
+//     console.log(n1 + n2)
+// }
  
-//convertendo node.list em array 
-const arrayElement = [document.querySelectorAll("li")];
 
-arrayElement.map( (el,key)=>{
-    if(el.textContent.indexOf("1",0)){
-        el.textContent = `${key+1} [item-10]`
-    }
-} )
+// //Recuperando elementos do HTML com querySelector
+// const liElements = document.querySelector("#item");
+// console.log(liElements);
 
-const divElement = document.getElementById('div-box')
-/*
-divElement.addEventListener("click", ()=>{
-    console.log(divElement)
-});
-*/
-divElement.addEventListener("click",mostraElemento);
-    function mostraElemento(){
-        console.log(divElement)
-    }
 
-    function soma (){
-        let v1 = 5
-        let v2 = 2
-        console.log(v1 + v2)
+// //Recuperando elementos do HTML com querySelectorAll
+// const liElements = document.querySelectorAll("li");
+// //CONVERTENDO NODE LIST EM ARRAY COM SPREAD
+// const arrayElements = [...liElements];
+// console.log(arrayElements);
 
-    }
+//CONVERTENDO NODE LIST EM ARRAY COM SPREAD
+// const arrayElements = [...document.querySelectorAll("li")];
+
+// arrayElements.map((el,key)=>{
+
+//     if (el.textContent.indexOf("1") != (-1)) {
+//         el.textContent = `[ ${el.textContent} ]`;
+//     }
+// });
+let tmp = 0;
+
+
+function mudaCor() {
+    let r = Math.ceil(Math.random()*255);
+    let g = Math.ceil(Math.random()*255);
+    let b = Math.ceil(Math.random()*255);
+    
+    const elemento = [...document.getElementsByClassName("conteudo")];
+    elemento.forEach( (el)=>{
+        el.style.backgroundColor = `rgb(${r},${g},${b})`;
+    } )
+
+   tmp = setTimeout(mudaCor, 5000);
+
+}
+
+function pararTimeOut() {
+    clearTimeout(tmp);
+}
+
+mudaCor()
