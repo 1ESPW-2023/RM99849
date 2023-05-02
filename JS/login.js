6a7467fda01d1586b389ffe3adc575031b37dbde
Carlosqmenezes <RM99849@fiap.com.br>
2023-05-02 20:36:18 -0300

    Eye

--- JS/login.js
+++ JS/login.js
@@ -2,10 +2,11 @@
 const inputUser = document.querySelector("input[type='text']");
 
 inputUser.addEventListener("focus", ()=>{
-    inputUser.setAttribute("style", "outline-color: #ff0000;")
-
+    inputUser.setAttribute("style","outline-color:#ff0000;")
 });
+
 inputUser.addEventListener("keyup", ()=>{
+
     const labelUser = document.querySelector("label[for='idNm']");
 
     if(inputUser.value.length < 5){
@@ -16,4 +17,19 @@ inputUser.addEventListener("keyup", ()=>{
         inputUser.setAttribute("style","outline-color:#00ff00;")
         labelUser.setAttribute("style","color:#00ff00;")
     }
+});
+
+const eyePass = document.querySelector(".fa-eye");
+const NotEyePass = document.querySelector(".fa fa-eye-slash")
+
+
+eyePass.addEventListener("click", ()=>{
+    const inputPass = document.querySelector("#idPass");
+
+    if(inputPass.getAttribute("type") == "password"){
+        inputPass.setAttribute("type","text");
+    }else{
+        inputPass.setAttribute("type","password");
+    }
+    
 });
