f9bf1e64adb0c895a10a9e50b67f2486be244ed0
Carlosqmenezes <RM99849@fiap.com.br>
2023-05-09 21:25:38 -0300

    alteração

--- JS/signin.js
+++ JS/signin.js
@@ -1,14 +1,54 @@
-const eyePass = document.querySelector(".eye");
+"use strict";
 
-eyePass.addEventListener("click", ()=>{
-    const inputPass = document.querySelector("#idPass");
+//CRIANDO UM OBJETO
+const usuario1 = {
+    nomeUsuario:"pt001",
+    senhaUsuario:"12345"
+}
 
-    if(inputPass.getAttribute("type") == "password"){
-        inputPass.setAttribute("type","text");
-        eyePass.setAttribute("class","fa fa-eye eye");
-    }else{
-        inputPass.setAttribute("type","password");
-        eyePass.setAttribute("class","fa fa-eye-slash eye");
+const usuario2 = {
+    nomeUsuario:"pt002",
+    senhaUsuario:"12345"
+}
+
+//CRIAR UMA LISTA DE OBJETOS
+let listaDeUsuarios = [];
+listaDeUsuarios.push(usuario1);
+listaDeUsuarios.push(usuario2);
+
+// listaDeUsuarios.forEach( (usuario)=>{
+    
+//     console.log("NOME DE USUÁRIO : " + usuario.nomeUsuario);
+//     console.log("SENHA DE USUÁRIO : " + usuario.senhaUsuario);
+
+// });
+
+
+addEventListener("click", (evt)=>{
+    if(evt.target.id == "btnSubmit"){
+
+        let usuarioInput = document.querySelector("#usuario").value;
+        let senhaInput = document.querySelector("#senha").value;
+
+
+        try{
+
+            listaDeUsuarios.forEach((usuario)=>{
+
+                if(usuarioInput == usuario.nomeUsuario && senhaInput == usuario.senhaUsuario){
+            
+                    throw "USUÁRIO VALIDADO!";
+
+                }else{
+
+                    throw "SENHA OU NOME DE USUÁRIO INVÁLIDO!";
+                }
+            });
+        }catch(err){
+            
+        }
+           
+
+
     }
-
 });
